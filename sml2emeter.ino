f354a9de550450b27ce559d8d9ae83da2e7b6631
Joern Tuemmler <Joern@tuemmler-net.de>
2019-09-14 02:34:21 +0200

    Integrate new SML parser

--- sml2emeter.ino
+++ sml2emeter.ino
@@ -17,6 +17,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
 #include "util/sml_testpacket.h"
+#include "util/smlparser.h"
 
 // ----------------------------------------------------------------------------
 // Compile time settings
@@ -31,42 +32,6 @@ const char VERSION[] = "Version 1.2";
 const bool USE_DEMO_DATA = false;
 
 // ----------------------------------------------------------------------------
-// SML constants
-// For details see:
-// https://www.bsi.bund.de/SharedDocs/Downloads/DE/BSI/Publikationen/TechnischeRichtlinien/TR03109/TR-03109-1_Anlage_Feinspezifikation_Drahtgebundene_LMN-Schnittstelle_Teilb.pdf?__blob=publicationFile
-// ----------------------------------------------------------------------------
-const uint32_t SML_ESCAPE = 0x1b1b1b1b;
-const uint32_t SML_VERSION1 = 0x01010101;
-const uint8_t SML_TAG_MASK = 0x70;
-const uint8_t SML_LENGTH_MASK = 0x0f;
-const uint8_t SML_LIST_TAG = 0x70;
-
-const uint8_t SML_ID_PATTERN[] = { 0x77, 0x07, 0x01, 0x00 };
-
-const int SML_MIN_SCALE = -2;
-const int SML_MAX_SCALE = 5;
-const int SML_SCALE_VALUES = SML_MAX_SCALE - SML_MIN_SCALE + 1;
-
-// Energy is in Ws                                        -2  -1   0     1      2       3        4         5
-const uint64_t SCALE_FACTORS_ENERGY[SML_SCALE_VALUES] = { 36, 360, 3600, 36000, 360000, 3600000, 36000000, 360000000 };
-
-// Power is in 0,1 W                                    -2  -1  0   1    2     3      4       5
-const int32_t SCALE_FACTORS_POWER[SML_SCALE_VALUES] = { -10, 1, 10, 100, 1000, 10000, 100000, 1000000 };
-
-// ----------------------------------------------------------------------------
-// OBIS constants
-//
-// For details see:
-// https://www.promotic.eu/en/pmdoc/Subsystems/Comm/PmDrivers/IEC62056_OBIS.htm
-// https://www.bundesnetzagentur.de/DE/Service-Funktionen/Beschlusskammern/BK06/BK6_81_GPKE_GeLi/Mitteilung_Nr_20/Anlagen/Obis-Kennzahlen-System_2.0.pdf?__blob=publicationFile&v=2
-// ----------------------------------------------------------------------------
-const uint8_t OBIS_INSTANTANEOUS_POWER_TYPE = 7;
-const uint8_t OBIS_ENERGY_TYPE = 8;
-const uint8_t OBIS_POSITIVE_ACTIVE_POWER = 1;
-const uint8_t OBIS_NEGATIVE_ACTIVE_POWER = 2;
-const uint8_t OBIS_SUM_ACTIVE_POWER = 16;
-
-// ----------------------------------------------------------------------------
 // SMA energy meter constants
 //
 // For details see:
@@ -78,6 +43,8 @@ const uint32_t SMA_POSITIVE_ACTIVE_POWER = 0x00010400;
 const uint32_t SMA_POSITIVE_REACTIVE_POWER = 0x00030400;
 const uint32_t SMA_NEGATIVE_ACTIVE_POWER = 0x00020400;
 const uint32_t SMA_NEGATIVE_REACTIVE_POWER = 0x00040400;
+const uint32_t SMA_POSITIVE_ENERGY = 0x00010800;
+const uint32_t SMA_NEGATIVE_ENERGY = 0x00020800;
 const uint32_t SMA_VERSION = 0x90000000;
 
 // IDs to identify offsets in the SMA meter header
@@ -127,16 +94,10 @@ const int CONFIG_PIN = D2;
 const int STATUS_PIN = LED_BUILTIN;
 
 const char INDEX_HTML[] = "<!doctype html><title>SML 2 EMeter</title><script>function u(n,t){var r=new XMLHttpRequest;r.onreadystatechange=function(){if(4==r.readyState&&200==r.status){var t=JSON.parse(r.responseText),e=\"\";for(var a in t)e=e.concat(\"<tr><td>{k}</td><td>{v}</td></tr>\".replace(\"{k}\",a).replace(\"{v}\",t[a]));document.getElementById(n).innerHTML=\"<table border><tr><th>Name</th><th>Value</th></tr>{d}</table>\".replace(\"{d}\",e)}},r.open(\"GET\",t,!0),r.send()}function r(){u(\"data\",\"data\")}function i(){r();self.setInterval(function(){r()},5e3)}</script><style>table{width:95%}th{background-color:#666;color:#fff}tr{background-color:#fffbf0;color:#000}tr:nth-child(odd){background-color:#e4ebf2}</style><body onload=i()><p>Current readings:<div id=data> </div><p>Change <a href=config>settings</a>.<p>{v}";
+
 // ----------------------------------------------------------------------------
 // Global variables
 // ----------------------------------------------------------------------------
-
-// Current meter reading
-uint32_t powerIn = 0;
-uint32_t powerOut = 0;
-uint32_t energyIn = 0;
-uint32_t energyOut = 0;
-uint32_t readErrors = 0;
 
 // Buffer for serial reading
 const int SML_PACKET_SIZE = 1000;
@@ -154,6 +115,11 @@ uint8_t *pMeterDataSize;
 
 // Pointer to the time field in the energy meter packet
 uint8_t *pMeterTime;
+
+// Parser for SML packets
+SmlParser smlParser;
+
+uint32_t readErrors = 0;
 
 // Destination addresses for sending meter packets
 const int DEST_ADRESSES_SIZE = 2;
@@ -271,129 +237,32 @@ void initEmeterPacket(uint32_t serNo) {
 }
 
 /**
-* @brief Read the next value from a sml packet
-* @note This Reader ignores lists tags. If a list occurs, it will
-*       return the next value after the list-tag.
+* @brief Update the energy meter packet
 */
-uint64_t getSmlValue(uint8_t *pPacket, int *pOffset) {
-   int p = *pOffset;
-   uint8_t type = pPacket[p] & SML_TAG_MASK;
-   while (type == SML_LIST_TAG) {
-      type = pPacket[++p] & SML_TAG_MASK;
-   }
-   uint8_t length = pPacket[p] & SML_LENGTH_MASK;
-   ++p;
-   uint64_t value = 0;
-   for (int i = 1; i < length; ++i) {
-      value = (value << 8) | pPacket[p++];
-   }
-   *pOffset = p;
-   return value;
-}
-
-/**
-* @brief Update the energy meter packet by analyzing the given sml packet
-*/
-uint16_t updateEmeterPacket(uint8_t *pSmlPacket, int smlLength) {
+uint16_t updateEmeterPacket() {
    uint8_t *pPacketPos = meterPacket + meterHeaderLength;
    storeU32BE(pMeterTime, millis());
 
    // Initial length of packet (ID + SN + TS)
    uint16_t length = INITIAL_PAYLOAD_LENGTH;
 
-   int patternPos = 0;
-   for (int i = 0; i < smlLength; ++i) {
-      if (pSmlPacket[i] == SML_ID_PATTERN[patternPos]) {
-         patternPos++;
-         if (patternPos == 4) {
-            // Get the SML values
-            uint8_t index = pSmlPacket[++i];
-            uint8_t type = pSmlPacket[++i];
-            uint8_t tariff = pSmlPacket[++i];
-            ++i; // Skip next value
-            uint32_t status = (uint32_t)getSmlValue(pSmlPacket, &i);
-            uint8_t timeType = (uint8_t)getSmlValue(pSmlPacket, &i);
-            uint32_t time = (uint32_t)getSmlValue(pSmlPacket, &i);
-            uint8_t unit = (uint8_t)getSmlValue(pSmlPacket, &i);
-            int32_t scale = (int32_t)getSmlValue(pSmlPacket, &i);
-            if (scale >= 0x80) {
-               scale -= 256;
-            }
-            uint64_t value = getSmlValue(pSmlPacket, &i);
-            patternPos = 0;
+   // Store active and reactive power (convert from centi-W to deci-W)
+   pPacketPos = storeU32BE(pPacketPos, SMA_POSITIVE_ACTIVE_POWER);
+   pPacketPos = storeU32BE(pPacketPos, smlParser.getPowerInW() / 10);
+   pPacketPos = storeU32BE(pPacketPos, SMA_NEGATIVE_ACTIVE_POWER);
+   pPacketPos = storeU32BE(pPacketPos, smlParser.getPowerOutW() / 10);
+   pPacketPos = storeU32BE(pPacketPos, SMA_POSITIVE_REACTIVE_POWER);
+   pPacketPos = storeU32BE(pPacketPos, 0);
+   pPacketPos = storeU32BE(pPacketPos, SMA_NEGATIVE_REACTIVE_POWER);
+   pPacketPos = storeU32BE(pPacketPos, 0);
+   length += 32;
 
-            // Store the value in the energy meter packet
-            if ((scale >= SML_MIN_SCALE) && (scale <= SML_MAX_SCALE)) {
-               // Calculate the index in the array of scale values
-               int scaleIndex = scale - SML_MIN_SCALE;
-
-               if (type == OBIS_INSTANTANEOUS_POWER_TYPE) {
-                  int32_t scaleFactor = SCALE_FACTORS_POWER[scaleIndex];
-                  int32_t intValue = (int32_t)value;
-                  if (scaleFactor < 0) {
-                     intValue /= (-scaleFactor);
-                  }
-                  else {
-                     intValue *= scaleFactor;
-                  }
-                  switch (index) {
-                  case OBIS_POSITIVE_ACTIVE_POWER:
-                     pPacketPos = storeU32BE(pPacketPos, SMA_POSITIVE_ACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, intValue);
-                     powerIn = intValue;
-                     pPacketPos = storeU32BE(pPacketPos, SMA_POSITIVE_REACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, 0);
-                     length += 16;
-                     break;
-                  case OBIS_NEGATIVE_ACTIVE_POWER:
-                     pPacketPos = storeU32BE(pPacketPos, SMA_NEGATIVE_ACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, intValue);
-                     powerOut = intValue;
-                     pPacketPos = storeU32BE(pPacketPos, SMA_NEGATIVE_REACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, 0);
-                     length += 16;
-                     break;
-                  case OBIS_SUM_ACTIVE_POWER:
-                     pPacketPos = storeU32BE(pPacketPos, SMA_POSITIVE_ACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, intValue >= 0 ? intValue : 0);
-                     powerIn = intValue >= 0 ? intValue : 0;
-                     pPacketPos = storeU32BE(pPacketPos, SMA_POSITIVE_REACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, 0);
-                     pPacketPos = storeU32BE(pPacketPos, SMA_NEGATIVE_ACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, intValue <= 0 ? -intValue : 0);
-                     powerOut = intValue <= 0 ? -intValue : 0;
-                     pPacketPos = storeU32BE(pPacketPos, SMA_NEGATIVE_REACTIVE_POWER);
-                     pPacketPos = storeU32BE(pPacketPos, 0);
-                     length += 32;
-                  }
-               }
-               if (type == OBIS_ENERGY_TYPE) {
-                  value *= SCALE_FACTORS_ENERGY[scaleIndex];
-                  *(pPacketPos++) = 0;
-                  *(pPacketPos++) = index;
-                  *(pPacketPos++) = type;
-                  *(pPacketPos++) = tariff;
-                  pPacketPos = storeU64BE(pPacketPos, value);
-                  length += 12;
-                  switch (index) {
-                  case OBIS_POSITIVE_ACTIVE_POWER:
-                     energyIn = (uint32_t)(value / 3600);
-                     break;
-                  case OBIS_NEGATIVE_ACTIVE_POWER:
-                     energyOut = (uint32_t)(value / 3600);                    
-                  }
-               }
-            }
-            else {
-               Serial.print("Error: Scale value out of bounds: ");
-               Serial.println(scale);
-            }
-         }
-      }
-      else {
-         patternPos = 0;
-      }
-   }
+   // Store energy (convert from centi-Wh to Ws)
+   pPacketPos = storeU32BE(pPacketPos, SMA_POSITIVE_ENERGY);
+   pPacketPos = storeU64BE(pPacketPos, smlParser.getEnergyInWh() * 36UL);
+   pPacketPos = storeU32BE(pPacketPos, SMA_NEGATIVE_ENERGY);
+   pPacketPos = storeU64BE(pPacketPos, smlParser.getEnergyOutWh() * 36UL);
+   length += 24;
 
    // Store version
    pPacketPos = storeU32BE(pPacketPos, SMA_VERSION);
@@ -478,15 +347,17 @@ void handleRoot()
 void handleData() {
    String data = "{";
    data += "\"PowerIn\" : ";
-   data += powerIn / 10.0;
+   data += smlParser.getPowerInW() / 100.0;
    data += ",\"EnergyIn\" : ";
-   data += energyIn / 1000.0;
+   data += smlParser.getEnergyInWh() / 100.0;
    data += ",\"PowerOut\" : ";
-   data += powerOut / 10.0;
+   data += smlParser.getPowerOutW() / 100.0;
    data += ",\"EnergyOut\" : ";
-   data += energyOut / 1000.0;
+   data += smlParser.getEnergyOutWh() / 100.0;
+   data += ",\"Ok\" : ";
+   data += (unsigned int)smlParser.getParsedOk();
    data += ",\"Errors\" : ";
-   data += readErrors;
+   data += (unsigned int)smlParser.getParseErrors() + readErrors;
    data += "}";
 
    server.send(200, "application/json", data);
@@ -614,10 +485,8 @@ void loop() {
 
    // Send the packet if a valid telegram was received
    if (smlPacketLength <= SML_PACKET_SIZE) {
-      uint32_t *pSmlHeader = (uint32_t*)smlPacket;
-      if ((pSmlHeader[0] == SML_ESCAPE) && (pSmlHeader[1] == SML_VERSION1)) {
-
-         int meterPacketLength = updateEmeterPacket(smlPacket, smlPacketLength);
+      if (smlParser.parsePacket(smlPacket, smlPacketLength)) {
+         int meterPacketLength = updateEmeterPacket();
          int i = 0;
          do {
             Serial.print("S");
